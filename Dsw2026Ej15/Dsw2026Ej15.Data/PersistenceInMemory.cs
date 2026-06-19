61ee3ea56c345d5c02fa20fb033ccb055712d437
MarianoRuiz <marianoruizcoronel@gmail.com>
2026-06-18 22:44:42 -0300

    Feat: Se agrego la persistencia

---
+++ Dsw2026Ej15/Dsw2026Ej15.Data/PersistenceInMemory.cs
@@ -0,0 +1,32 @@
+﻿using Dsw2026Ej15.Domain;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Dsw2026Ej15.Data;
+
+public class PersistenceInMemory : IPersistence
+{
+    public List<Speciality> Specialities { get; private set; }
+    public List<Doctor> Doctors { get; private set; }
+
+    public PersistenceInMemory()
+    {
+   
+        Specialities = new List<Speciality>();
+        Doctors = new List<Doctor>();
+        LoadSpecialities().Wait();
+    }
+    private async Task LoadSpecialities()
+    {
+        if (File.Exists("specialities.json"))
+        {
+            var json = await File.ReadAllTextAsync("specialities.json");
+            Specialities = JsonSerializer.Deserialize<List<Speciality>>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true 
+            });
+        }
+    }
+}
