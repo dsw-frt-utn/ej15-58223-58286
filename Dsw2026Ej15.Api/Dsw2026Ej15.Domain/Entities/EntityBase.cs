30bf08251f05d282836ab8395a06ef61ba8e0f1a
Franco Reyes <francoreyes597@gmail.com>
2026-06-18 22:20:49 -0300

    Se definió la estructura y las entidades

---
+++ Dsw2026Ej15.Api/Dsw2026Ej15.Domain/Entities/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Domain.Entities
+{
+    public class EntityBase()
+    {
+        public Guid? _id = Guid.NewGuid();
+        public EntityBase(Guid? id) :this()
+        {
+            _id = id;
+        }
+    }
+}
